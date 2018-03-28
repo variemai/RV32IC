c2f6e484fd1f7bca97285b27976e8c4a8c645328
Ioannis Vardas <ffsoras@gmail.com>
2018-03-28 19:18:04 +0300

    comments

--- Decoder.sv
+++ Decoder.sv
@@ -1,3 +1,10 @@
+/*****************************************************************************
+ * The Instruction Decoder module, creates the control signals,              *
+ * reads from the register file and stores the information to EX_STATE reg   *
+ * Authors: Vardas Ioannis                                                   *
+ * created for the purposes of CS-523 RISC-V 32 bit implementation project   *
+ * Computer Science Department University of Crete 27/03/2018                *
+ *****************************************************************************/
 `include "PipelineRegs.v"
 module decoder(
     input clk,
--- PipelineRegs.v
+++ PipelineRegs.v
@@ -1,3 +1,9 @@
+/*****************************************************************************
+ * Pipeline Registers used by the modules                                    *
+ * Authors: Vardas Ioannis                                                   *
+ * created for the purposes of CS-523 RISC-V 32 bit implementation project   *
+ * Computer Science Department University of Crete 27/03/2018                *
+ *****************************************************************************/
 package PipelineRegs;
 
     typedef struct packed {
