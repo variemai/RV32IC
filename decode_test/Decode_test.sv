1071214784d18eef943fa8d533afb97f7a175028
John Vardas <vardas@socofreta.ics.forth.gr>
2018-04-04 15:55:15 +0300

    loop test

--- decode_test/Decode_test.sv
+++ decode_test/Decode_test.sv
@@ -7,23 +7,27 @@ program decode_test(
 	output PipelineReg::ID_STATE id_state
 );
 	logic [31:0] pc = 31'b0;
+	integer i;
 	initial begin
-		issue_r_instruction(pc);
-		//pc = pc + 4;
+		for(i=0; i<2; i++) begin
+			@(posedge clk) begin
+				issue_instruction(pc,32'b00000000000100010000000000110011);
+				pc <= pc + 4;
+			end
+		end
 	end
 
 
-	task issue_r_instruction(bit [31:0] pc);
-		@(posedge clk) begin
-			id_state.instruction <= 32'b00000000000100010000000000110011;
-			id_state.pc <= pc;
-		end
+	task issue_instruction(bit [31:0] _pc,bit [31:0] instruction);
+		//@(posedge clk) begin
+			id_state.instruction = instruction;
+			id_state.pc = _pc;
+		//end
 		@(posedge clk) begin
 			assert(ex_state.rd == 5'b0);
 			assert(ex_state.rs1 == 5'b10);
 			assert(ex_state.rs2 == 5'b01);
-			assert(ex_state.pc == 5'b0);
-			assert(ex_state.pc == pc);
+			assert(ex_state.pc == _pc);
 			assert(ex_state.func3 == 3'b0);
 			assert(ex_state.func7 == 7'b0);
 		end
--- decode_test/csrc/filelist.cu
+++ decode_test/csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _4769_archive_1.so
-_4769_archive_1.so : archive.0/_4769_archive_1.a
+ARCHIVE_OBJS += _8374_archive_1.so
+_8374_archive_1.so : archive.0/_8374_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_4769_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_8374_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_4769_archive_1.so $@
+	@ln -sf .//../simv.daidir//_8374_archive_1.so $@
 
 
 
