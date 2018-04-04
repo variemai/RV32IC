d866a495fe3d92bfc674bbf2c7e92ae57c14982f
John Vardas <vardas@socofreta.ics.forth.gr>
2018-04-04 17:15:45 +0300

    working sram initialize from file

--- sram.sv
+++ sram.sv
@@ -1,56 +1,28 @@
-module Imem(
-    clk,
-    rst,
-    read_rq,
-    write_rq,
-    rw_address,
-    write_data,
-    read_data
-);
-input           clk;
-input           rst;
-input           read_rq;
-input           write_rq;
-input[5:0]      rw_address;
-input[7:0]      write_data;
-output[7:0]     read_data;
+module sram
+#(
+//------------------------------------------------
+parameter ADDR_WIDTH = 9,
+parameter DATA_WIDTH = 32,
+parameter RAM_SIZE = 512
+//------------------------------------------------
+)   (
+	input clk,
+	input we,
+	input [ADDR_WIDTH-1:0] addr,
+	input [DATA_WIDTH-1:0] din,
+	output [DATA_WIDTH-1:0] dout
+	);
 
-reg[7:0]     read_data;
-
-integer out, i;
-
-// Declare memory 64x8 bits = 512 bits or 64 bytes
-reg [7:0] memory_ram_d [63:0];
-reg [7:0] memory_ram_q [63:0];
-
-// Use positive edge of clock to read the memory
-// Implement cyclic shift right
-always @(posedge clk or
-    negedge rst)
-begin
-    if (!rst)
-    begin
-        for (i=0;i<64; i=i+1)
-            memory_ram_q[i] <= 0;
-    end
-    else
-    begin
-        for (i=0;i<64; i=i+1)
-             memory_ram_q[i] <= memory_ram_d[i];
-    end
-end
-
-
-always @(*)
-begin
-    for (i=0;i<64; i=i+1)
-        memory_ram_d[i] = memory_ram_q[i];
-    if (write_rq && !read_rq)
-        memory_ram_d[rw_address] = write_data;
-    if (!write_rq && read_rq)
-        read_data = memory_ram_q[rw_address];
-end
-
+	logic [DATA_WIDTH-1:0] RAM [RAM_SIZE-1:0];
+	logic [DATA_WIDTH-1:0] dout;
+	initial begin
+		$readmemb("instructions.data", RAM, 0, 10);
+	end
+	always @(posedge clk) begin
+		if(we)
+			RAM[addr] <= din;
+		else
+			dout <= RAM[addr];
+	end
+	
 endmodule
-
-
