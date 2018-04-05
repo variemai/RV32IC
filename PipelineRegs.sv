fe647b4a31e4ee89b5223add704ccc8b0ed12373
John Vardas <vardas@socofreta.ics.forth.gr>
2018-04-05 15:55:55 +0300

    decoder development

--- PipelineRegs.sv
+++ PipelineRegs.sv
@@ -21,11 +21,12 @@ package PipelineReg;
         logic [31:0] pc;
         logic [2:0] func3; 
 		logic [6:0] func7;
+		logic [1:0] ALUOp; //described in book page 253 
         logic [31:0] rs1_data;
         logic [31:0] rs2_data;
         logic [31:0] immediate; //not sure about this
         logic [4:0] rd;
-        logic alu_src;
+		logic ALUsrc;
         /*More signals for forwarding and hazard detection*/
         logic [4:0] rs1;
         logic [4:0] rs2;
--- decode_test/Decoder.sv
+++ decode_test/Decoder.sv
@@ -16,13 +16,28 @@ module decoder(
 	
 	always @(posedge clk) begin
 		casez(id_state.instruction)
-			`ADD: begin
+			`ADD, `SUB, `SLL, `SLT, `SLTU, `XOR, `SRL, `SRA, `OR, `AND: 
+			begin
 				ex_state.pc <= id_state.pc;
 				ex_state.rd <= id_state.instruction[11:7];
 				ex_state.rs1 <= id_state.instruction[19:15];
 				ex_state.rs2 <= id_state.instruction[24:20];
 				ex_state.func3 <= id_state.instruction[14:12];
 				ex_state.func7 <= id_state.instruction[31:25];
+				ex_state.ALUOp <= 2'b10;
+				ex_state.ALUsrc <= 0;
+			end
+			`ADDI, `SLTI, `SLTIU, `XORI, `ORI, `ANDI, `SLLI, `SRLI, `SRAI:
+			begin
+				ex_state.pc <= id_state.pc;
+				ex_state.rd <= id_state.instruction[11:7];
+				ex_state.rs1 <= id_state.instruction[19:15];
+				ex_state.func3 <= id_state.instruction[14:12];
+				ex_state.ALUOp <= 2'b11;
+				ex_state.ALUsrc <= 1;
+				//TODO WORK ON THIS
+				ex_state.immediate <= {{16{id_state.instruction[31]},id_state.instruction[31:20}};
+
 			end
 			default: begin
 				$write("Unknown Instruction Format!\n");
